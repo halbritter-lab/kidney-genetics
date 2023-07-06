4c0ba1a7f298e634d3a1084258a0460c0ced0503
Bernt Popp <bernt.popp@gmail.com>
2023-07-06 12:05:45 +0200

    add gnomad annotation function

---
+++ analyses/functions/gnomad-functions.R
@@ -0,0 +1,89 @@
+require(httr)
+require(jsonlite)
+require(tidyverse)
+
+#' Fetch Gene Data from gnomAD GraphQL API
+#'
+#' This function takes an Ensemble gene identifier as input, makes a POST request
+#' to the gnomAD GraphQL API, and returns the gene data as a tibble.
+#'
+#' @param ensemble_id A character string representing the Ensemble gene identifier.
+#'
+#' @return A tibble with columns for each field returned by the API. This includes
+#'         fields such as gene_id, gene_version, symbol, and others, as well as nested
+#'         fields under gnomad_constraint.
+#'
+#' @examples
+#' \dontrun{
+#'   gene_data <- getGeneData("ENSG00000008710")
+#'   print(gene_data)
+#' }
+#'
+#' @export
+get_gene_data_from_gnomad <- function(ensemble_id) {
+
+  # API URL
+  api_url <- "https://gnomad.broadinstitute.org/api/"
+
+  # Request Body
+  body <- list(
+    query = paste0(
+      '{
+        gene(gene_id: "', ensemble_id, '", reference_genome: GRCh37) {
+          reference_genome
+          gene_id
+          gene_version
+          symbol
+          gencode_symbol
+          name
+          canonical_transcript_id
+          hgnc_id
+          ncbi_id
+          omim_id
+          chrom
+          start
+          stop
+          strand
+          gnomad_constraint {
+            exp_lof
+            exp_mis
+            exp_syn
+            obs_lof
+            obs_mis
+            obs_syn
+            oe_lof
+            oe_lof_lower
+            oe_lof_upper
+            oe_mis
+            oe_mis_lower
+            oe_mis_upper
+            oe_syn
+            oe_syn_lower
+            oe_syn_upper
+            lof_z
+            mis_z
+            syn_z
+            pLI
+          }
+        }
+      }'
+    )
+  )
+
+  # Send POST request
+  response <- POST(api_url, body = body, encode = "json")
+
+  # Check if request was successful
+  if (response$status_code == 200) {
+    # Parse the JSON response
+    data <- fromJSON(content(response, "text", encoding = "UTF-8"))
+
+    # Convert the nested list to a flat data frame
+    df <- as.data.frame(data$data$gene)
+
+    # Convert the data frame to a tibble and return
+    return(as_tibble(df))
+  } else {
+    stop("Request failed with status code ", response$status_code)
+  }
+}
