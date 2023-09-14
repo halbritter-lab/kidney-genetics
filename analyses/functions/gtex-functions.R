8ec1fe763dde02961942d11680039e4e7885efb5
Bernt Popp <bernt.popp@gmail.com>
2023-09-14 18:47:30 +0200

    add functions to compute genocode id

--- analyses/functions/gtex-functions.R
+++ analyses/functions/gtex-functions.R
@@ -86,3 +86,105 @@ get_multiple_median_tissue_expression <- function(gencode_ids, max_ids_per_reque
   # Return the aggregated tibble
   return(aggregated_data)
 }
+
+
+#' Get GENCODE IDs from GTEx API
+#'
+#' This function takes a list of gene symbols as input, makes a GET request
+#' to the GTEx API, and returns the first GENCODE ID encountered for each
+#' gene symbol as a tibble. If the API returns an empty response, NA is returned 
+#' for the gencode_id.
+#'
+#' @param gene_symbols A character vector representing the gene symbols.
+#'
+#' @return A tibble with columns: gene_symbol and gencode_id.
+#'
+#' @examples
+#' \dontrun{
+#'   gencode_ids <- get_gencode_ids(c("PKD1", "PKD2"))
+#'   print(gencode_ids)
+#' }
+#' @export
+get_gencode_ids <- function(gene_symbols) {
+  
+  # Prepare the API URL
+  base_url <- "https://gtexportal.org/api/v2/reference/gene?"
+  ids_query <- paste0("geneId=", gene_symbols, collapse = "&")
+  full_url <- paste0(base_url, ids_query)
+
+  # Make the API request
+  response <- GET(full_url)
+
+  # Check if request was successful
+  if (http_status(response)$category != "Success") {
+    stop("Failed to retrieve data from GTEx API.")
+  }
+
+  # Parse the JSON response
+  parsed_data <- fromJSON(content(response, "text", encoding = "UTF-8"))
+
+  # Create a tibble from the input gene symbols
+  output_tibble <- tibble(gene_symbol = gene_symbols)
+
+  # Check if the response contains data
+  if (length(parsed_data$data) > 0) {
+    # Create a tibble from the parsed data
+    response_tibble <- as_tibble(parsed_data$data)
+
+    # Join the input tibble with the response tibble to ensure all input gene
+    # symbols appear in the output and select only the first GENCODE ID for
+    # each gene symbol if multiple are returned.
+    output_tibble <- output_tibble %>%
+      left_join(response_tibble, by = c("gene_symbol" = "geneSymbol")) %>%
+      group_by(gene_symbol) %>%
+      slice(1) %>%
+      ungroup() %>%
+      dplyr::select(gene_symbol, gencode_id = gencodeId)
+  } else {
+    # If the response is empty, add a column with NA values for the gencode_id
+    output_tibble <- output_tibble %>%
+      mutate(gencode_id = NA_character_)
+  }
+
+  return(output_tibble)
+}
+
+
+#' Fetch GENCODE IDs from GTEx API for Multiple Gene Symbols
+#'
+#' This function takes a character vector of gene symbols as input, splits it 
+#' into appropriate chunks to fit the API input size limitations, makes a GET 
+#' request for each chunk of gene symbols to the GTEx API, and returns the 
+#' GENCODE IDs as a tibble.
+#'
+#' @param gene_symbols A character vector representing the gene symbols.
+#' @param max_symbols_per_request An integer specifying the maximum number of 
+#'        gene symbols that can be queried in a single API request. Default is 50.
+#'
+#' @return A tibble with rows for each gene and a column containing the 
+#'         GENCODE ID returned by the GTEx API.
+#'
+#' @examples
+#' \dontrun{
+#'   gencode_ids <- get_multiple_gencode_ids(c("PKD1", "PKD2"))
+#'   print(gencode_ids)
+#' }
+#' \dontrun{
+#'   gencode_ids <- get_multiple_gencode_ids(c("PKD1", "PKD2"), max_symbols_per_request = 25)
+#'   print(gencode_ids)
+#' }
+#' @export
+get_multiple_gencode_ids <- function(gene_symbols, max_symbols_per_request = 50) {
+
+  # Split the gene_symbols into chunks that fit within the API's limitations
+  symbol_chunks <- split(gene_symbols, ceiling(seq_along(gene_symbols) / max_symbols_per_request))
+
+  # Use purrr::map to iterate over symbol_chunks and get a list of tibbles
+  list_of_tibbles <- purrr::map(symbol_chunks, get_gencode_ids)
+
+  # Use purrr::reduce to bind all tibbles into a single tibble
+  aggregated_data <- purrr::reduce(list_of_tibbles, dplyr::bind_rows)
+
+  # Return the aggregated tibble
+  return(aggregated_data)
+}
