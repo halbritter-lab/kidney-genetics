b09b6ab0a4bcd6f1bd2b577d4f8d00ba15dead5d
Bernt Popp <bernt.popp@gmail.com>
2023-10-07 17:11:11 +0200

    update mpo functions

--- analyses/functions/mpo-mousemine-functions.R
+++ analyses/functions/mpo-mousemine-functions.R
@@ -1,83 +1,80 @@
-require(ontologyIndex)
-require(InterMineR)
+require(ontologyIndex) #
+require(InterMineR) # Load the InterMineR package before using the functions
 
-
-#' Retrieve the MP children terms for a given MP term ID.
+#### This file holds analyses functions for MPO request
+#' Retrieve the MPO children terms for a given MPO term ID.
 #'
-#' This function retrieves the children terms of a given term from the Mammalian
-#' Phenotype Ontology (MPO) using the ontologyIndex package.
+#' This function retrieves the immediate children terms of a specified term 
+#' from the Mammalian Phenotype Ontology (MPO). It uses the `ontologyIndex` package 
+#' to interact with the ontology and the `tidyr` package to format the results.
 #'
-#' @param term_input_id The MP term ID for which to retrieve the children terms.
+#' @param term_input_id The MPO term ID for which to retrieve the children terms. 
+#'   This should be a valid MPO term ID string.
+#'
+#' @details The function starts by querying the ontology for the children of the 
+#'   given term using `get_term_property()`. The result is then transformed into 
+#'   a tibble. Additionally, the current system date is added as an attribute 
+#'   to each term, representing the date of the query.
 #'
 #' @importFrom ontologyIndex get_term_property
 #' @importFrom tidyr as_tibble
 #'
-#' @return A tibble with the MP children terms corresponding to the input MP term ID.
+#' @return A tibble with two columns: 
+#'   - `term`: Contains the MPO children terms corresponding to the input MPO term ID.
+#'   - `query_date`: Contains the date when the query was made.
 #'
 #' @examples
 #' \dontrun{
-#' # Load the Mammalian Phenotype Ontology (MPO) before using the function
-#' # Example usage of the function:
-#' mp_children_from_term("MP:0005502")
+#'   # Load the Mammalian Phenotype Ontology (MPO) before using the function
+#'   # Example usage of the function:
+#'   mpo_children_from_term("MP:0005502")
 #' }
 #'
 #' @export
-mp_children_from_term <- function(term_input_id) {
-  # Use get_term_property to retrieve the children of the given term
-  children_terms <- get_term_property(ontology = mpo, property = "children",
-                                      term = term_input_id)
+mpo_children_from_term <- function(term_input_id) {
+  # Query the ontology for children of the given term
+  children_terms <- get_term_property(ontology = mpo, property = "children", term = term_input_id)
 
-  # Convert the list of children terms to a tibble
-  children_tibble <- tidyr::as_tibble(data.frame(ontology_id = children_terms))
+  # Convert the result into a tibble and add the current query date
+  children_tibble <- children_terms %>%
+    tidyr::as_tibble() %>%
+    mutate(query_date = Sys.Date()) %>%
+    select(term = value, query_date)
 
   return(children_tibble)
 }
 
 
-#' Retrieve all MP descendants and the term itself from term ID.
+#' Retrieve all MPO descendants and the term itself from term ID.
 #'
-#' This function retrieves all the MP children terms, including nested children,
-#' for a given MP term ID using the Mammalian Phenotype Ontology (MPO).
-#'
-#' @param term_input_id The MP term ID for which to retrieve all children terms.
-#' @param all_children_list A list to accumulate all children terms. Should be empty at the initial call.
-#'
-#' @importFrom tidyr as_tibble
-#'
-#' @return A tibble with all the MP children terms, including nested children,
-#'   corresponding to the input MP term ID.
-#'
+#' @param term_input_id The MPO term ID for which to retrieve all children terms.
+#' @param all_children_list A list to accumulate all children terms. Initial call should be empty.
+#' @return A tibble with unique MPO children terms and the query date, including nested children,
+#'   corresponding to the input MPO term ID.
 #' @examples
 #' \dontrun{
-#' # Load the Mammalian Phenotype Ontology (MPO) before using the function
+#' # Load the Human Phenotype Ontology (MPO) before using the function
 #' # Example usage of the function with an empty list as the initial call:
-#' mp_all_children_from_term("MP:0005502", list())
+#' mpo_all_children_from_term("MP:0005502")
 #' }
-#'
 #' @export
-mp_all_children_from_term <- function(term_input_id, all_children_list = list()) {
+mpo_all_children_from_term <- function(term_input_id, all_children_list = tibble()) {
 
   # Retrieve the immediate children of the current term
-  children_tibble <- mp_children_from_term(term_input_id)
+  children_tibble <- mpo_children_from_term(term_input_id)
 
-  # Combine all_children_list and term_input_id
-  all_children_list <- c(all_children_list, list(term_input_id))
+  # Add the current term to the results
+  all_children_list <- bind_rows(all_children_list, tibble(term = term_input_id, query_date = Sys.Date()))
 
-  if (nrow(children_tibble) != 0) {
-    for (child_id in children_tibble$ontology_id) {
-      # Update all_children_list with each recursive call
-      all_children_list <- mp_all_children_from_term(child_id, all_children_list)
+  # If there are children, recursively get their children
+  if (nrow(children_tibble) > 0) {
+    for (child_id in children_tibble$term) {
+      all_children_list <- mpo_all_children_from_term(child_id, all_children_list)
     }
   }
 
-  # If no more children are found, flatten the list, convert it to a tibble and remove duplicates
-  if (nrow(children_tibble) == 0) {
-    all_children_vector <- unlist(all_children_list, use.names = FALSE)
-    all_children_tibble <- tidyr::as_tibble(data.frame(ontology_id = unique(all_children_vector)))
-    return(all_children_tibble)
-  } else {
-    return(all_children_list)
-  }
+  # Return only unique rows
+  return(distinct(all_children_list))
 }
 
 
